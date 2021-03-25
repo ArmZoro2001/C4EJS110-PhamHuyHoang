bea384c11cd685820d7c897aebbba51c2de62382
ArmZoro2001 <sang19720@gmail.com>
2021-03-26 01:19:50 +0700

    Homeword

---
+++ Session 4/index.js
@@ -0,0 +1,20 @@
+// let person =['Nam',19, 'Hà Nội', 25, false]
+// // console.log(person)
+
+// let person ={};
+// console.log(person)
+
+// let person = {
+//     name: 'Nam'
+// };
+// console.log(person)
+
+// let person = {
+//     name: 'Nam',
+//     age: 19,
+// };
+// console.log(person);
+// console.log(person.age);
+// let prop = 'age';
+// console.log(person['name']);
+// person.age++;
